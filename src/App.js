d9728356cf3d2e2e9691acb7517c915b38ffdead
mestrace <yida.liu@case.edu>
2021-09-18 18:10:51 +0800

    add semantic ui theme
    Change-Id: I03217185e4933aa62cf9d4af69df5fd83c97f0bf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import React, { Component } from "react";
 import { Input, Menu, Container, Segment } from "semantic-ui-react";
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "semantic-ui-css/semantic.min.css";
+
+import "semantic-ui-less/semantic.less";
 
 ReactDOM.render(
   <React.StrictMode>
