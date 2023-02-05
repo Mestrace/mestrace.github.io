76ec2f56f1cf1a0b52806fc56e5ea2586d5ae22f
Yida Liu <yida.liu@qq.com>
2023-02-05 23:56:55 +0800

    setup theme/Flex

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+
 AUTHOR = "Mestrace"
 SITENAME = "mestrace.github.io"
 SITEURL = ""
+SITETITLE = "Mestrace"
+SITESUBTITLE = "Software Developer"
+SITEDESCRIPTION = "My Thoughts"
+SITELOGO = "https://avatars.githubusercontent.com/u/26028388?v=4"
+BROWSER_COLOR = "#333"
+PYGMENTS_STYLE = "monokai"
+
 
 PATH = "content"
 
@@ -15,16 +24,65 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+THEME = "themes/Flex"
+
 # # Blogroll
 # LINKS = (('Pelican', 'https://getpelican.com/'),
 #          ('Python.org', 'https://www.python.org/'),
 #          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
 #          ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (("https://github.com/Mestrace", "#"),)
 
+STATIC_PATHS = ["images"]
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+I18N_TEMPLATES_LANG = "en"
+DEFAULT_LANG = "en"
+OG_LOCALE = "en_US"
+LOCALE = "en_US"
+
+# code blocks with line numbers
+PYGMENTS_RST_OPTIONS = {"linenos": "table"}
+
+# there is no other HTML content
+READERS = {"html": None}
+
+GITHUB_URL = "http://github.com/Mestrace/"
+GITHUB_CORNER_URL = GITHUB_URL
+MAIN_MENU = True
+SOCIAL = (("github", GITHUB_URL),)
+
+
+ARTICLE_URL = "posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/"
+ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html"
+
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike 4.0 International License",
+    "version": "4.0",
+    "slug": "by-sa",
+    "icon": True,
+    "language": "en_US",
+}
+
+COPYRIGHT_YEAR = datetime.now().year
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+
+# Enable i18n plugin.
+# PLUGINS = ["i18n_subsites"]
+# # Enable Jinja2 i18n extension used to parse translations.
+# JINJA_ENVIRONMENT = {"extensions": ["jinja2.ext.i18n"]}
+MARKDOWN = {
+    "extension_configs": {
+        "markdown.extensions.codehilite": {"css_class": "highlight"},
+        "markdown.extensions.extra": {},
+        "markdown.extensions.meta": {},
+    },
+}
+
+
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+USE_LESS = True
